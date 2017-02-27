3abddb0c1f676b09b3103db9f7c449692b495cf0
Sriramajeyam <yesoreyeram@gmail.com>
2017-02-27 12:00:05 +0000

    Update install_updates.sh

--- ubuntu/install_updates.sh
+++ ubuntu/install_updates.sh
@@ -4,6 +4,9 @@
 # Copyright (c) 2016 sriram
 # **************************************************************************
 
-echo "Hello Linux User"
-echo ""
-echo {command -v java}
+sudo apt-get update && sudo apt-get upgrade --yes
+wget https://grafanarel.s3.amazonaws.com/builds/grafana_4.1.2-1486989747_amd64.deb
+sudo apt-get install -y adduser libfontconfig
+sudo dpkg -i grafana_4.1.2-1486989747_amd64.deb
+sudo service grafana-server start
+curl "localhost:3000"
